133948ff856da79e8adbcfc597ddb36ae4f679e0
sanjaylakum7 <sanjaylakum431@gmail.com>
2024-03-15 10:52:35 +0530

    added all

---
+++ src/main/java/com/example/simple_one/SimpleOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOneApplication.class, args);
+	}
+
+}
